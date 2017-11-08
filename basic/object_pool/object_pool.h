7de5578636242a318ab64f54981071c385f0aecf
LIYI99 <1377176144@qq.com>
2017-11-08 02:23:48 -0800

    LIYI add object_pool

---
+++ basic/object_pool/object_pool.h
@@ -0,0 +1,50 @@
+#ifndef __OBJECT_POOL_H__
+#define __OBJECT_POOL_H__
+
+#include <pthread.h>
+
+#ifdef __cplusplus
+    extern "C"{
+#endif
+
+
+
+typedef struct object_pool_s object_pool_t;
+typedef (void) (*object_node_init)(void *data);
+
+struct  qnode {
+    unsigned char tab; 
+    struct qonde    *prev;
+    struct qonde    *next;
+};
+
+struct object_pool_s {
+    unsigned int        object_size;
+    unsigned int        max;
+    unsigned int        usecnts;
+    object_node_init    _init;
+
+    struct  qnode       *head;
+    struct  qnode       *end;
+    void                *pool;
+
+};
+
+
+
+object_pool_t*  object_pool_create(unsigned int object_size,unsigned int max,
+        object_node_init *func);
+void    object_pool_destory(object_pool_t *s);
+void*   object_pool_get(object_pool_t *s);
+void*   object_pool_free(object_pool_t *s);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+#endif
